9f7b1f8d666b536a6245a31f993622769b11810f
Pavel Kishov <kishov.p@gmail.com>
2022-06-09 18:53:25 +0300

    made a code

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+//int number1 = int.Parse(Console.ReadLine());
+
+int sqrNumber = number*number;
+//int sqrNumber1 = Convert.ToInt32(Math.Pow(number1,2));
+
+WriteLine(sqrNumber);
