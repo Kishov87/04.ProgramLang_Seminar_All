acd3ff5c8434c548d1fc20c484a4b385a29e960a
Pavel Kishov <kishov.p@gmail.com>
2022-06-10 19:08:19 +0300

    add Task9 ready

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -1,3 +1,18 @@
 ﻿//**Задача 16:**
 //Напишите программу, которая принимает на вход два числа и 
 //проверяет, является ли одно число квадратом другого.
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число a = ");
+int a = Convert.ToInt32(ReadLine());
+Write("Введите число b = ");
+int b = Convert.ToInt32(ReadLine());
+
+string answer = (a/b == b) || (b/a == a)
+?"Одно число является квадратом другого"
+:$"Числа не являются квадратами друг друга";
+
+WriteLine(answer);
