24b64fd02893a44afce3ddfbda1178c9a09de990
Pavel Kishov <kishov.p@gmail.com>
2022-06-10 18:34:03 +0300

    add Task6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -4,3 +4,13 @@
 //456 -> 46
 //782 -> 72
 //918 -> 98
+
+using System;
+using static System.Console;
+
+Clear();
+
+int number = new Random().Next(100,1000);
+WriteLine($"Случайное число = {number}");
+
+WriteLine($"{number/100}{number%10}{" - юпиии"}");
