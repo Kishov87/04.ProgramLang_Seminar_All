5141b17a420861a4d609355d265694a7c5d586fd
Pavel Kishov <kishov.p@gmail.com>
2022-06-10 19:08:05 +0300

    add Task8 ready

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -4,3 +4,26 @@
 //14 -> нет
 //46 -> нет
 //161 -> да
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int number1 = Convert.ToInt32(ReadLine());
+int a1 = 7;
+int a2 = 23;
+
+int N1 = number1%a1;
+int N2 = number1%a2;
+
+//if ((number1 < a1) || (number1 < a2))
+//{
+//    WriteLine("Нужно чтобы число было больше 23! ");
+//    return;
+//}
+string answer1 = N1 ==0 & N2 == 0 
+?"Число кратное 23 и 7 одновременно"
+:$"Число не кратное 23 и 7.";
+
+WriteLine(answer1);
