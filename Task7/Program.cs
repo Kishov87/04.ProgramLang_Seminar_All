7e0d3059636c5aad4b342fae595a7d018318fc57
Pavel Kishov <kishov.p@gmail.com>
2022-06-10 18:34:11 +0300

    add Task7

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -4,3 +4,24 @@
 //то программа выводит остаток от деления.
 //34, 5 -> не кратно, остаток 4
 //16, 4 -> кратно
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите первое число: ");
+int number1 = Convert.ToInt32(ReadLine());
+Write("Введите второе число: ");
+int number2 = Convert.ToInt32(ReadLine());
+int N = number1%number2;
+
+if (number1<number2)
+{
+    WriteLine("Нужно чтобы первое число было больше второго! ");
+    return;
+}
+string answer = N == 0 
+?"Число кратное"
+:$"Число не кратное. Остаток = {N}";
+
+WriteLine(answer);
