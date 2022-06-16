836c75ce3be6171f0e76ef76b24fa1ab7fc95f28
Pavel Kishov <kishov.p@gmail.com>
2022-06-16 22:05:10 +0300

    add Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число 
+//и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+using System;
+using static System.Console; 
+
+Clear();
+Write("Введите число: ");
+WriteLine($"Количество цифр = {CountNumb(Convert.ToInt32(ReadLine()))}");
+
+int CountNumb(int r)
+{
+    if (r == 0) return 1;
+    int count=0;
+    for(count=0; r!=0; count++)
+    {
+        r = r/10;
+    }
+    return count;
+}
