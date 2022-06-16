0eccc19485f9528bc41677f0cd6331f6e10a862a
Pavel Kishov <kishov.p@gmail.com>
2022-06-16 22:05:18 +0300

    add Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,22 @@
+﻿//**Задача 28:**
+//Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.
+//4  -> 24
+//5  -> 120
+
+using System;
+using static System.Console; 
+
+Clear();
+Write("Введите число: ");
+WriteLine($"Произведение чисел = {Getn(Convert.ToInt32(ReadLine()))}");
+
+int Getn(int n)
+{
+    int result = 1;
+    for(int i=1; i<=n; i++)
+    {
+        result*=i;
+    }
+    return result;
+}
