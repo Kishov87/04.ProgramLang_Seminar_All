fe13071c4b0c96524e9bca77df7b8f0de2b50bb7
Pavel Kishov <kishov.p@gmail.com>
2022-06-16 22:05:02 +0300

    add Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,28 @@
+﻿//**Задача 24:**
+//Напишите программу, которая принимает на вход число (А) 
+//и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+using System;
+using static System.Console; 
+
+Clear();
+Write("Введите число: ");
+// int N = Convert.ToInt32(ReadLine());
+
+// int sum = GetSum(N);
+// WriteLine($"Сумма чисел = {sum}");
+
+WriteLine($"Сумма чисел = {GetSum(Convert.ToInt32(ReadLine()))}");
+
+int GetSum(int r)
+{
+    int result = 0;
+    for(int i=1; i<=r; i++)
+    {
+        result+=i;
+    }
+    return result;
+}
