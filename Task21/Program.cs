743034b3005b0e0c773a70f8a63e8a655f5b3c6d
Pavel Kishov <kishov.p@gmail.com>
2022-06-28 19:53:45 +0300

    add finished Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,40 @@
+﻿//**Задача 33:** Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+//4; массив [6, 7, 19, 345, 3] -> нет
+//3; массив [6, 7, 19, 345, 3] -> да
+
+using System;
+using static System.Console; 
+Clear();
+
+Write("Введите размер массива: ");
+int size = int.Parse(ReadLine());
+int[] array = GetRandomArray(size,-5,5);
+WriteLine($"массив [{String.Join(", ", array)}]");
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+WriteLine($"массив [{String.Join(", ", array)}] -> {FindNumber(array, number)} {number}");
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result=new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue +1);
+    }
+    return result;
+}
+
+string FindNumber(int[] array, int numbers)
+{
+    string flag = "не содержит число:";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == numbers)
+        {
+            flag = "содержит число:";
+            break; 
+        }
+    }    
+    return flag;
+}
