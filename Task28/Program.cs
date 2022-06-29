1c1fb0fbce99b6400c5845fa7cb2b0cd7cf73f3a
Pavel Kishov <kishov.p@gmail.com>
2022-06-29 20:37:19 +0300

    add finished Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 45: Напишите программу, которая будет 
+// создавать копию заданного массива с помощью поэлементного копирования.
+
+using System;
+using static System.Console; 
+Clear();
+
+Write("Введите элементы массива через пробел: ");
+int[] array = GetArrayFromString(ReadLine());
+WriteLine($"[{String.Join(", ", array)}]");
+WriteLine($"[{String.Join(", ", Array(array))}]");
+
+int[] GetArrayFromString(string arrayStr)
+{
+    string[] arS=arrayStr.Split(new char[]{' ',',','*'},StringSplitOptions.RemoveEmptyEntries);
+    int[] result=new int[arS.Length];
+    for(int i=0; i<arS.Length;i++)
+    {
+        result[i]=int.Parse(arS[i]);
+    }
+    return result;
+}
+
+int[] Array(int[] array)
+{
+    int[] result=new int[array.Length];
+    for(int i = 0; i<array.Length;i++)
+    {
+        result[i]=array[i];
+    }
+    return result;
+}
