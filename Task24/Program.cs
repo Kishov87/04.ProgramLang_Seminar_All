6b14ad1364a7bf1c375b52a9ea17d3bdb0bf6220
Pavel Kishov <kishov.p@gmail.com>
2022-06-29 19:39:06 +0300

    add finished Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,44 @@
+﻿// **Задача 39:** Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+using System;
+using static System.Console; 
+Clear();
+
+Write("Введите элементы массива через пробел: ");
+int[] array = GetArrayFromString(ReadLine());
+WriteLine($"[{String.Join(", ", array)}]");
+WriteLine($"[{String.Join(", ", ReverseArray(array))}]");
+
+int[] GetArrayFromString(string arrayStr)
+{
+    string[] arS=arrayStr.Split(new char[]{' ',',','*'},StringSplitOptions.RemoveEmptyEntries);
+    int[] result=new int[arS.Length];
+    for(int i=0; i<arS.Length;i++)
+    {
+        result[i]=int.Parse(arS[i]);
+    }
+    return result;
+}
+
+int[] ReverseArray(int[] array)
+{
+    int[] result=new int[array.Length];
+    for(int i = 0; i<array.Length;i++)
+    {
+        result[i]=array[array.Length-i-1];
+    }
+    return result;
+}
+
+// void ReverseArray1(int[] array)
+// {
+//     for(int i=0; i<array.Length/2;i++)
+//     {
+//         int k=array[i];
+//         array[i]=array[array.Length-i-1];
+//         array[array.Length-i-1]=k;
+//     }
+// }
