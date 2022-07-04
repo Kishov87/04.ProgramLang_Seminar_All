4bd859a9a698a6628b9ab56dc35a5cb141bca0fb
Pavel Kishov <kishov.p@gmail.com>
2022-07-04 21:24:38 +0300

    add Task33

---
+++ Task33/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача на одномерный массив поиск числа, например MAX и MIN или сумму
+
+using System;
+using static System.Console;
+
+Clear();
+
+int[] array={1,2,3,4,5};
+int max=array[0];
+int min=array[0];
+int sum=0;
+int count=0;
+
+foreach(var r in array)
+{
+    max=max<r?r:max;
+    min=min>r?r:min;
+    sum+=r;
+    count+=r%2==0?1:0;
+}
+
+WriteLine($"{min} {max} {sum} {count}");
+
+WriteLine(String.Join(",",GetArrayFrom("1 2 3")));
+
+int[] GetArrayFrom(string strAr)
+{
+    string[] arS=strAr.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+    int[] result=new int[arS.Length];
+    int i=0;
+    foreach(var item in arS)
+    {
+        result[i]=int.Parse(item);
+        i++;
+    }
+    return result;
+}
